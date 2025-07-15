feedd11acf41a90f60445c5e9bac194a38824b6b
Bartosz Ciulkin <bartosz.ciulkin@gmail.com>
2025-07-15 21:49:15 +0200

    Refactoring (#4)
    * Refactoring
    
    * Red border
    
    * Update go.yml
    
    * Add direction
    
    * Fixed

--- model/fish.go
+++ model/fish.go
@@ -6,10 +6,43 @@ import (
 	"fmt"
 )
 
+type DirectionEnum int
+
+const (
+	Right DirectionEnum = iota
+	Left
+)
+
+func direction() DirectionEnum {
+	if (rand.Intn(2) == 1) {
+		return Right
+	} else {
+		return Left
+	}
+}
+
 type Fish struct {
 	X, Y   int
 	Symbol string
 	Speed  int
+	Direction DirectionEnum
+}
+
+func NewFish(width, height int) Fish {
+	f := Fish{
+		X:      rand.Intn(width - 1) + 1,
+		Y:      rand.Intn(height - 1) + 1,
+		Speed:  rand.Intn(2) + 1,
+		Direction: direction(),
+	}
+
+	if (f.Direction == Right) {
+		f.Symbol = []string{"><>", "><((º>"}[rand.Intn(2)]
+	} else {
+		f.Symbol = []string{"<><", "<º))><"}[rand.Intn(2)]
+	}
+	
+	return f
 }
 
 func (f *Fish) DrawFish() {
@@ -20,7 +53,11 @@ func (f *Fish) DrawFish() {
 }
 
 func (f *Fish) UpdateFish() {
-	f.X += f.Speed
+	if (f.Direction == Right) {
+		f.X += f.Speed
+	} else {
+		f.X -= f.Speed
+	}
 	// Random vertical movement
 	if rand.Intn(10) > 7 {
 		f.Y += rand.Intn(3) - 1 // -1, 0 or +1
